93dd4b1026123090be800aaf30ca6e96858efe42
MadDeCoDeR <gkwin8ac@hotmail.gr>
2018-09-07 14:44:04 +0300

    - Improved linux cmake scripts

--- neo/cmake-eclipse-linux-retail.sh
+++ neo/cmake-eclipse-linux-retail.sh
@@ -1,5 +1,24 @@
+clear
+echo --------------------------------------------------------------------------------
+COLUMNS=$(tput cols) 
+title="CLASSIC RBDOOM 3 BFG EDITION" 
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+COLUMNS=$(tput cols) 
+title="ECLIPSE UNIX MakeFile GENERATION FOR x86 - x64" 
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title" 
+COLUMNS=$(tput cols) 
+title="RETAIL BUILD" 
+printf "%*s\n" $(((${#title}+$COLUMNS)/2)) "$title"
+echo --------------------------------------------------------------------------------
 cd ..
-rm -rf build
-mkdir build
-cd build
-cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Retail -DSDL2=ON ../neo
+rm -rf build > /dev/null 2>&1
+mkdir build > /dev/null 2>&1
+cd build 
+echo Generating files
+cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Retail -DSDL2=ON ../neo > /dev/null 2>&1 ||(
+echo ERROR Generating files
+cd ..
+rmdir /Q /S build
+read -rsp $'Press any key to continue...\n' -n1 key
+)
+read -rsp $'Press any key to continue...\n' -n1 key
