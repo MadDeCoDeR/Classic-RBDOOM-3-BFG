4c56ae444b63d28cd95f2f61410d8234a3bb8ae4
MadDeCoDeR <gkwin8ac@hotmail.gr>
2019-02-15 16:51:02 +0200

    Version Update

--- docs/table.js
+++ docs/table.js
@@ -1,4 +1,4 @@
-var table='<table align="left" id="toc"><tbody><tr><th>Table of Contents</th></tr><tr><td><a href="index.html">Home Page</a></td></tr><tr><td><a href="update.html">Changelog</a></td></tr><tr><td><a href="manual.html">Tutorials</a></td></tr><tr><td><a href="video.html">Videos</a></td></tr><tr><td><a href="download.html">Download Links</a></td></tr></tbody></table>';
+var table ='<table align="left" id="toc"><tbody><tr><th>Table of Contents</th></tr><tr><td><a href="index.html">Home Page</a></td></tr><tr><td><a href="update.html">Changelog</a></td></tr><tr><td><a href="manual.html">Tutorials</a></td></tr><tr><td><a href="video.html">Videos</a></td></tr><tr><td><a href="download.html">Download Links</a></td></tr><tr><td><a href="https://www.patreon.com/GK4EVER">Donations</a></td></tr></tbody></table>';
 
 
 document.getElementById("body").innerHTML+= table;
