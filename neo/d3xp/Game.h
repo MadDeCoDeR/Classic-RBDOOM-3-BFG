48c11eeeba38f12de8258032e121223ea2b3d93e
MadDeCoDeR <gkwin8ac@hotmail.gr>
2019-11-02 19:01:56 +0200

    Update SDK Version

--- neo/d3xp/Game.h
+++ neo/d3xp/Game.h
@@ -403,7 +403,7 @@ extern idGameEdit* 				gameEdit;
 */
 //GK: Distinct OG dll from BFG dll
 //Update: added support for Classic Doom
-const int GAME_API_VERSION		= 11;
+const int GAME_API_VERSION		= 12;
 
 typedef struct
 {
