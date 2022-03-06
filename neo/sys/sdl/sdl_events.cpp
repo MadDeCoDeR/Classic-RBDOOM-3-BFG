95aee92f4be3e7e4ebb0c7831af5550013dec090
MadDeCoDeR <gkwin8ac@hotmail.gr>
2022-03-06 23:03:46 +0200

    Remove laggy SDL Event condition

--- neo/sys/sdl/sdl_events.cpp
+++ neo/sys/sdl/sdl_events.cpp
@@ -1271,14 +1271,11 @@ sysEvent_t Sys_GetEvent()
 				{K_JOY_DPAD_LEFT, SDL_CONTROLLER_BUTTON_DPAD_LEFT},
 				{K_JOY_DPAD_RIGHT, SDL_CONTROLLER_BUTTON_DPAD_RIGHT},
 			};
-			if (buttonStates[controllerButtonRemap[ev.cbutton.button][0]] != ev.cbutton.state) {
-				res.evType = SE_KEY;
-				res.evValue = controllerButtonRemap[ev.cbutton.button][0];
-				res.evValue2 = ev.cbutton.state == SDL_PRESSED ? 1 : 0;
+			res.evType = SE_KEY;
+			res.evValue = controllerButtonRemap[ev.cbutton.button][0];
+			res.evValue2 = ev.cbutton.state == SDL_PRESSED ? 1 : 0;
 
-				joystick_polls.Append(joystick_poll_t(res.evValue, res.evValue2));
-				buttonStates[controllerButtonRemap[ev.cbutton.button][0]] = ev.cbutton.state;
-			}
+			joystick_polls.Append(joystick_poll_t(res.evValue, res.evValue2));
 			return res;
 #else
 			// WM0110
