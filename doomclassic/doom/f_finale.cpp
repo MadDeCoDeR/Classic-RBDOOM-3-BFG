f8ac8df489d67d48371dd771503b962ca49a8c18
MadDeCoDeR <gkwin8ac@hotmail.gr>
2022-04-21 17:53:15 +0300

    GCC 11 sineniganians (aka it throws error because %i can give bigger numbers than expected)

--- doomclassic/doom/f_finale.cpp
+++ doomclassic/doom/f_finale.cpp
@@ -1065,7 +1065,7 @@ void F_BunnyScroll (void)
     int		x;
     patch_t*	p1;
     patch_t*	p2;
-    char	name[10];
+    char	name[13];
     int		stage;
 		
     p1 =  /*(patch_t*)*/img2lmp(W_CacheLumpName ("PFUB2", PU_FINALE), W_GetNumForName("PFUB2"));
