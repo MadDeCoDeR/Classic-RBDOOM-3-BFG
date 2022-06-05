47220aacd90defbeda8efa739b1e59dfe96e59a6
George Kalampokis <gkwin8ac@hotmail.gr>
2022-06-05 12:14:29 +0300

    Fix Path where FFMPEG files will be stored

--- .github/tools/download-ffmpeg.ps1
+++ .github/tools/download-ffmpeg.ps1
@@ -1,6 +1,6 @@
 $ffmpegName = "ffmpeg-n5.0-latest-win64-lgpl-shared-5.0";
 Invoke-WebRequest -Method Get -Uri "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/$ffmpegName.zip" -OutFile "ffmpeg.zip";
 Expand-Archive -Path "ffmpeg.zip" .;
-Copy-Item -Filter *.dll -Path "$ffmpegName\bin" -Destination "../../build" -Recurse;
+Copy-Item -Filter *.dll -Path "$ffmpegName\bin" -Destination "../../build/ffmpeg" -Recurse;
 Remove-Item -Path "ffmpeg.zip";
 Remove-Item -Path "$ffmpegName" -Recurse;
