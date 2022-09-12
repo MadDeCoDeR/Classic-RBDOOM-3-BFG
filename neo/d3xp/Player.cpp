0ad095192962d6d27477654a5ad42dc2927c3810
MadDeCoDeR <gkwin8ac@hotmail.gr>
2022-09-12 19:37:32 +0300

    Fix nexprevweapon keys on DOOM 3 (ref #89)

--- neo/d3xp/Player.cpp
+++ neo/d3xp/Player.cpp
@@ -7940,7 +7940,7 @@ void idPlayer::EvaluateControls()
 	//GK: Custom made weapon cycle algorithm in order to handle press and hold events
 	int npw = 0;
 	bool isIntroMap = (idStr::FindText(gameLocal.GetMapFileName(), "mars_city1") >= 0);
-	if (usercmd.buttons & BUTTON_PREVWEAP && tweap == currentWeapon && circleWeaponPacifier >= (com_engineHz_latched / 2))
+	if (usercmd.buttons & BUTTON_NEXTWEAP && tweap == currentWeapon && circleWeaponPacifier >= (com_engineHz_latched / 2))
 	{
 		if (!isIntroMap)
 		{
@@ -7964,7 +7964,7 @@ void idPlayer::EvaluateControls()
 			circleWeaponPacifier = 0;
 		}
 	}
-	if (usercmd.buttons & BUTTON_NEXTWEAP && tweap == currentWeapon && circleWeaponPacifier >= (com_engineHz_latched / 2))
+	if (usercmd.buttons & BUTTON_PREVWEAP && tweap == currentWeapon && circleWeaponPacifier >= (com_engineHz_latched / 2))
 	{
 		if (!isIntroMap)
 		{
