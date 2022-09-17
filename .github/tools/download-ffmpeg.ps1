45c7ecf149e69f63774b423ecaaef48f572bf036
George Kalampokis <gkwin8ac@hotmail.gr>
2022-09-17 14:25:17 +0300

    Fix versioning numbers for download ffmpeg script

--- .github/tools/download-ffmpeg.ps1
+++ .github/tools/download-ffmpeg.ps1
@@ -1,4 +1,4 @@
-$ffmpegName = "ffmpeg-n5.0-latest-win64-lgpl-shared-5.0";
+$ffmpegName = "ffmpeg-n5.1-latest-win64-lgpl-shared-5.1";
 Invoke-WebRequest -Method Get -Uri "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/$ffmpegName.zip" -OutFile "ffmpeg.zip";
 Expand-Archive -Path "ffmpeg.zip" .;
 Copy-Item -Filter *.dll -Path "$ffmpegName\bin" -Destination "../../build/ffmpeg" -Recurse;
