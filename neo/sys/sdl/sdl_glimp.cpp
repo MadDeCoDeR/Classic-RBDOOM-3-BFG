7f329a43a302be2fa1afb5b9b7e7ab437019dd1b
MadDeCoDeR <gkwin8ac@hotmail.gr>
2023-02-27 16:43:21 +0200

    Possible multimonitor Fix for Linux

--- neo/sys/sdl/sdl_glimp.cpp
+++ neo/sys/sdl/sdl_glimp.cpp
@@ -739,55 +739,42 @@ bool R_GetModeListForDisplay( const unsigned requestedDisplayNum, idList<vidMode
 	modeList.Clear();
 #if SDL_VERSION_ATLEAST(2, 0, 0)
 	// DG: SDL2 implementation
-	if( requestedDisplayNum >= SDL_GetNumVideoDisplays() )
-	{
-		// requested invalid displaynum
-		return false;
-	}
-	
-	int numModes = SDL_GetNumDisplayModes( requestedDisplayNum );
-	if( numModes > 0 )
-	{
-		for( int i = 0; i < numModes; i++ )
+	int displayNum = 0;
+	for (int j = 0; j < SDL_GetNumVideoDisplays(); j++) {
+		modeList.Clear();
+		if (j > requestedDisplayNum)
 		{
-			SDL_DisplayMode m;
-			int ret = SDL_GetDisplayMode( requestedDisplayNum, i, &m );
-			if( ret != 0 )
+			break;
+		}
+
+		int numModes = SDL_GetNumDisplayModes(j);
+		if (numModes > 0)
+		{
+			for (int i = 0; i < numModes; i++)
 			{
-				common->Warning( "Can't get video mode no %i, because of %s\n", i, SDL_GetError() );
-				continue;
+				SDL_DisplayMode m;
+				int ret = SDL_GetDisplayMode(j, i, &m);
+				if (ret != 0)
+				{
+					common->Warning("Can't get video mode no %i, because of %s\n", i, SDL_GetError());
+					continue;
+				}
+				if (m.h < 720) {
+					continue;
+				}
+
+				vidMode_t mode;
+				mode.width = m.w;
+				mode.height = m.h;
+				mode.displayHz = m.refresh_rate ? m.refresh_rate : 60; // default to 60 if unknown (0)
+				modeList.AddUnique(mode);
 			}
-			if (m.h < 720) {
-				continue;
-			}
-			
-			vidMode_t mode;
-			mode.width = m.w;
-			mode.height = m.h;
-			mode.displayHz = m.refresh_rate ? m.refresh_rate : 60; // default to 60 if unknown (0)
-			modeList.AddUnique( mode );
+
+			// sort with lowest resolution first
+			modeList.SortWithTemplate(idSort_VidMode());
 		}
-		
-		if( modeList.Num() < 1 )
-		{
-			common->Warning( "Couldn't get a single video mode for display %i, using default ones..!\n", requestedDisplayNum );
-			FillStaticVidModes( modeList );
-		}
-		
-		// sort with lowest resolution first
-		modeList.SortWithTemplate( idSort_VidMode() );
 	}
-	else
-	{
-		common->Warning( "Can't get Video Info, using default modes...\n" );
-		if( numModes < 0 )
-		{
-			common->Warning( "Reason was: %s\n", SDL_GetError() );
-		}
-		FillStaticVidModes( modeList );
-	}
-	
-	return true;
+	return modeList.Num() > 0;
 	// DG end
 	
 #else // SDL 1
