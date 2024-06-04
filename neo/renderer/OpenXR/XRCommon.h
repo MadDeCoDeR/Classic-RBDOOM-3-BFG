da39b95a4415e035ef341c684925e706e9a17cf7
MadDeCoDeR <18539489+MadDeCoDeR@users.noreply.github.com>
2024-06-04 22:11:49 +0300

    Add extension and api layer lookups (following OpenXR tutorial)

--- neo/renderer/OpenXR/XRCommon.h
+++ neo/renderer/OpenXR/XRCommon.h
@@ -4,5 +4,19 @@
 #include "openxr/openxr_platform.h"
 #include "gfxwrapper_opengl.h"
 
-
-void InitXR();
+class idXR {
+public:
+	void InitXR();
+private:
+	XrInstance instance = {};
+	std::vector<std::string> activeAPILayers = {};
+	std::vector<std::string> activeExtensions = {};
+	std::vector<std::string> APILayers = {};
+	std::vector<std::string> extensions = {};
+#ifdef _DEBUG
+	XrDebugUtilsMessengerEXT debugMessager = {};
+#endif
+	XrFormFactor formFactor = XR_FORM_FACTOR_HEAD_MOUNTED_DISPLAY;
+	XrSystemId systemId = {};
+	XrSystemProperties systemProperties = { XR_TYPE_SYSTEM_PROPERTIES };
+};
--- neo/sys/win32/win_oxr.cpp
+++ neo/sys/win32/win_oxr.cpp
@@ -7,7 +7,7 @@
 #include <vector>
 
 
-void InitXR() {
+void idXR::InitXR() {
 	XrApplicationInfo XRAppInfo;
 	strncpy(XRAppInfo.applicationName, "DOOM BFA", 8);
 	XRAppInfo.apiVersion = XR_CURRENT_API_VERSION;
@@ -15,10 +15,51 @@ void InitXR() {
 	XRAppInfo.engineVersion = atoi(ENGINE_VERSION);
 	strncpy(XRAppInfo.engineName, "DOOM BFA", 8);
 
-	std::vector<std::string> extentions;
 #ifdef _DEBUG
-	extentions.push_back(XR_EXT_DEBUG_UTILS_EXTENSION_NAME);
+	this->extensions.push_back(XR_EXT_DEBUG_UTILS_EXTENSION_NAME);
 #endif
-	extentions.push_back(XR_KHR_OPENGL_ENABLE_EXTENSION_NAME);
+	this->extensions.push_back(XR_KHR_OPENGL_ENABLE_EXTENSION_NAME);
 
+	uint apiLayerCount = 0;
+	std::vector<XrApiLayerProperties> apiLayerProperties;
+	if (xrEnumerateApiLayerProperties(0, &apiLayerCount, nullptr) == XR_SUCCESS) {
+		apiLayerProperties.resize(apiLayerCount, { XR_TYPE_API_LAYER_PROPERTIES });
+		if (xrEnumerateApiLayerProperties(apiLayerCount, &apiLayerCount, apiLayerProperties.data()) == XR_SUCCESS) {
+			for (std::string& requestLayer : APILayers) {
+				for (XrApiLayerProperties& layerProperty : apiLayerProperties) {
+					if (idStr::Icmp(requestLayer.c_str(), layerProperty.layerName)) {
+						continue;
+					}
+					else {
+						activeAPILayers.push_back(requestLayer.c_str());
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	uint extensionCount = 0;
+	std::vector<XrExtensionProperties> extensionProperties;
+	if (xrEnumerateInstanceExtensionProperties(nullptr, 0, &extensionCount, nullptr) == XR_SUCCESS) {
+		extensionProperties.resize(extensionCount, { XR_TYPE_EXTENSION_PROPERTIES });
+		if (xrEnumerateInstanceExtensionProperties(nullptr, extensionCount, &extensionCount, extensionProperties.data()) == XR_SUCCESS) {
+			for (std::string& requestExtension : extensions) {
+				bool found = false;
+				for (XrExtensionProperties& extensionProperty : extensionProperties) {
+					if (idStr::Icmp(requestExtension.c_str(), extensionProperty.extensionName)) {
+						continue;
+					}
+					else {
+						activeExtensions.push_back(requestExtension.c_str());
+						found = true;
+						break;
+					}
+				}
+				if (!found) {
+					common->Error("Failed to find OpenXR Extension: %s\n", requestExtension);
+				}
+			}
+		}
+	}
 }
