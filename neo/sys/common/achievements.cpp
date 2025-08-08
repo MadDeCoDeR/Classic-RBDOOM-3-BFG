ee64b6a2f0d75e6ffcee15baa6be86c09d8be9ef
MadDeCoDeR <18539489+MadDeCoDeR@users.noreply.github.com>
2025-08-08 17:07:56 +0300

    Achievement - Fix logging

--- neo/sys/common/achievements.cpp
+++ neo/sys/common/achievements.cpp
@@ -67,7 +67,7 @@ void idAchievementSystemWin::AchievementUnlock( idLocalUser* user, int achieveme
 #ifndef ALLOW_DEV
 	if (::op) {
 		idStr achievementName = GetAchievementName(achievementID);
-		common->Printf("Trying to unlock ach '%s' with id '%d'\n");
+		common->Printf("Trying to unlock ach '%s' with id '%d'\n", achievementName.c_str(), achievementID);
 		::op->openAchievement()->UnlockAchievement(achievementName);//va("%s%d", STEAM_ACHIEVEMENT_PREFIX, achievementID));
 	}
 #endif
